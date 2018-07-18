be1e7930b2a924001c5da14e38875d15364c4ed5
Daniel Ye <danielye4@gmail.com>
2018-07-18 18:28:55 -0400

    added Projects section and animated thumbnails

--- main.js
+++ main.js
@@ -3,37 +3,41 @@ $(document).ready(() => {
 	$('.jumbotron .row h3').fadeIn(600);
 	$('.gallery .gallery-item .thumbnail').on('mouseenter', event => {
 		$(event.currentTarget).animate({
-			opacity:1
-		},350);
+			opacity:1,
+			height:"23rem",
+			width: "32rem"
+		},290);
 	});
 	$('.gallery .thumbnail').on('mouseleave', event => {
 		$(event.currentTarget).animate({
-			opacity:0.8
-		},350);
+			opacity:0.9,
+			height:"21rem",
+			width: "30rem"
+		},290);
 	});
 	$('.gallery #academics').on('click', () => {
 		$('#academics').fadeOut(500);
-		$('.academicsinfo').slideDown(500); 
+		$('.academicsinfo').delay(500).slideDown(500); 
 	});
 	$('.gallery #sports').on('click', () => {
 		$('#sports').fadeOut(500);
-		$('.sportsinfo').slideDown(500); 
+		$('.sportsinfo').delay(500).slideDown(500); 
 	});
 	$('.gallery #extracurriculars').on('click', () => {
 		$('#extracurriculars').fadeOut(500);
-		$('.extrainfo').slideDown(500); 
+		$('.extrainfo').delay(500).slideDown(500); 
 	});
 	$('.academicsinfo .closebutton').on('click', () => {
 		$('.academicsinfo').slideUp(500); 
-		$('#academics').fadeIn(500);	
+		$('#academics').delay(500).fadeIn(500);	
 	});
 	$('.sportsinfo .closebutton').on('click', () => {
 		$('.sportsinfo').slideUp(500); 
-		$('#sports').fadeIn(500);	
+		$('#sports').delay(500).fadeIn(500);	
 	});
 	$('.extrainfo .closebutton').on('click', () => {
 		$('.extrainfo').slideUp(500); 
-		$('#extracurriculars').fadeIn(500);	
+		$('#extracurriculars').delay(500).fadeIn(500);	
 	});
 	function goToByScroll(id){
 	          // Remove "link" from the ID
